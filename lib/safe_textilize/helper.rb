88b56466899d53f02214b3ec6420834553d3afd5
Stefan Kroes <s.a.kroes@nedforce.nl>
2012-11-20 16:59:12 +0100

    Added attributes to sanitizer

--- lib/safe_textilize/helper.rb
+++ lib/safe_textilize/helper.rb
@@ -6,12 +6,7 @@ module SafeTextilize
       
       return '' if text.blank?
      
-      Sanitize.clean(RedCloth.new(text, options).to_html, :transformers => [SafeTextilize::VideoTransformer.instance], :elements => %w[
-        a abbr b bdo blockquote br caption cite code col colgroup dd del div dfn dl
-        dt em figcaption figure h1 h2 h3 h4 h5 h6 hgroup i img ins kbd li mark
-        ol p pre q rp rt ruby s samp small strike strong sub sup table tbody td
-        tfoot th thead time tr u ul var wbr
-      ])    
+      Sanitize.clean(RedCloth.new(text, options).to_html, SafeTextilize::SANITIZER.merge(:transformers => [SafeTextilize::VideoTransformer.instance])
     end
     
   end
